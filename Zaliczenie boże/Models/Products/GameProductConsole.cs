ea718c41414f20db679620328dc6e4c6b2d95ce6
01dejwid <98768935+01dejwid@users.noreply.github.com>
2023-01-30 02:07:51 +0100

    Add files via upload

---
+++ Zaliczenie boże/Models/Products/GameProductConsole.cs
@@ -0,0 +1,21 @@
+﻿using Zaliczenie.Types;
+
+namespace Zaliczenie.Models.Products
+{
+    public class GameProductConsole : Product
+    {
+        public GameProductConsole(string name, float price, GameTypeConsole gameType) : base(name, price)
+        {
+            Name = name;
+            Price = price;
+            GameType = gameType;
+        }
+
+        public GameTypeConsole GameType { get; }
+
+        public override void DisplayProduct(int number)
+        {
+            Console.WriteLine($"{number}. {Name}, Price: {Price}PLN, Game type: {GameType}");
+        }
+    }
+}
